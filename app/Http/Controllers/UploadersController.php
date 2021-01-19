d1546e190ea32225a13b839606ee99a6c775255c
sumitaka-sakuma <tksn.playthe69gt2@gmail.com>
2021-01-19 23:00:22 +0900

    added compact method

--- app/Http/Controllers/UploadersController.php
+++ app/Http/Controllers/UploadersController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Uploader;
 
 class UploadersController extends Controller
 {
     public function index(){
 
-        return view('uploaders/index');
+        $uploader = Uploader::orderBy('created_at', 'desc')->paginate(5);
+        return view('uploaders/index', compact('uploader'));
     }
 }
